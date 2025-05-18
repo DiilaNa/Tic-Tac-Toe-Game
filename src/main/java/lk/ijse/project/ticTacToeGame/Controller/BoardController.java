48b475cb73c7412d2dd0122021d46414141d0cea
DiilaNa <liyanaarachchidilan@gmail.com>
2025-05-18 20:47:34 +0530

    Board Controller UI added

--- src/main/java/lk/ijse/project/ticTacToeGame/Controller/BoardController.java
+++ src/main/java/lk/ijse/project/ticTacToeGame/Controller/BoardController.java
@@ -1,4 +1,53 @@
 package lk.ijse.project.ticTacToeGame.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class BoardController {
+
+    @FXML
+    private Button b1;
+
+    @FXML
+    private Button eight;
+
+    @FXML
+    private Button five;
+
+    @FXML
+    private Button four;
+
+    @FXML
+    private Button nine;
+
+    @FXML
+    private Button one;
+
+    @FXML
+    private Button seven;
+
+    @FXML
+    private Button six;
+
+    @FXML
+    private Button three;
+
+    @FXML
+    private Button two;
+
+    @FXML
+    private Label userName;
+
+    @FXML
+    void ButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonAction(ActionEvent event) {
+
+    }
+
 }
