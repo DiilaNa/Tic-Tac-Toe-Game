6964b7fda6c21b7aed635e1dd839503e54ec0976
DiilaNa <liyanaarachchidilan@gmail.com>
2025-05-18 21:00:09 +0530

    exported package to javafx.graphics in module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module lk.ijse.project.ticTacToeGame {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens lk.ijse.project.ticTacToeGame.Controller to javafx.fxml;
     exports lk.ijse.project.ticTacToeGame.Controller;
+    exports lk.ijse.project.ticTacToeGame.Game;
+    exports lk.ijse.project.ticTacToeGame;
+    opens lk.ijse.project.ticTacToeGame.Game to javafx.fxml;
 }
